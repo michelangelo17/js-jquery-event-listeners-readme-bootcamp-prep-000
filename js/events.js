80b9b5970880ac4964faed31fbe60f9903ab475d
Michelangelo Markus <michelangelo.markus@gmail.com>
2018-02-23 02:33:22 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  }
+)}
+
+function frameIt() {
+  $('img').on('load');
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  $('input').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('You have pressed the letter "G"');
+    }
+  } 
+)}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  }
+)}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
